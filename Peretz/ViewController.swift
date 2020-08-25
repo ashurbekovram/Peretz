db9ab56d3204963725bbe44dd0ab3c3ba47a7a8e
ramzisdead <ramzisdead@gmail.com>
2020-08-26 02:01:25 +0300

    Initial Commit

---
+++ Peretz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Peretz
+//
+//  Created by Рамазан on 26.08.2020.
+//  Copyright © 2020 Рамазан. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
